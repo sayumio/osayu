4a3a83ad3816bc9c656a2e15305cdc3384240d45
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-23 21:08:46 +0900

    validatesの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-         validates :nickname
-         validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
- end
+        validates :nickname
+        validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+        validates :password, format: { with: /\A[a-zA-Z0-9]+\z/},length: { minimum: 6}
+        validates :sei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+        validates :mei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+        validates :kanasei, format: { with: /\A[ァ-ヶー－]+\z/}
+        validates :kanamei, format: { with: /\A[ァ-ヶー－]+\z/}
+        validates :birthday
+   end
