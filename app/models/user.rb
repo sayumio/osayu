ddb678bff4f9a5d76422eb9bc855d5badb52ed1b
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-24 13:27:30 +0900

    validates整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
         validates :nickname
-        validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
-        validates :password, format: { with: /\A(?=.*?[a-zA-Z0-9])+{1,1}+\z/},length: { minimum: 6}
+        validates :password, format: { with: /\A(?=.*?[a-zA-Z0-9])+{1,1}+\z/}
         validates :sei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
         validates :mei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
         validates :kanasei, format: { with: /\A[ァ-ヶー－]+\z/}
