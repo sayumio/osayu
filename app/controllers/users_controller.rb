8e9327ad15ce055c1a9f9d4dc415df40dcd81f23
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-25 22:55:23 +0900

    エラー発生時にメッセージを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create(user_params)
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :managements
+
+  validates :name, presence: true
 end
