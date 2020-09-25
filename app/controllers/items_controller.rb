367cd319787bf24eeb1735779e37b35002c9f27d
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-25 17:23:15 +0900

    未ログインユーザーの転送

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
