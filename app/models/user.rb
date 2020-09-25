78029ec22755ad4196eea4a2d4b1f5d5f9ccb21f
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-25 16:09:04 +0900

    validatesの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence: true do
-        validates :nickname
-        validates :password, format: { with: /\A(?=.*?[a-zA-Z0-9])+{1,1}+\z/}
-        validates :sei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-        validates :mei, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-        validates :kanasei, format: { with: /\A[ァ-ヶー－]+\z/}
-        validates :kanamei, format: { with: /\A[ァ-ヶー－]+\z/}
-        validates :birthday
    end
 end
