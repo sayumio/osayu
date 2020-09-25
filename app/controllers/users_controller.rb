9b2e40de707248847bd32dd63c89d0e283266927
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-25 14:57:22 +0900

    MVCの整理（トップページを表示させるまで）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
   end
 
-  def new
-    @user =User.new
-  end
+  #def new
+  #  @user =User.new
+  #end
 
-  def create
-    User.create(user_params)
-  end
+  #def create
+  #  User.create(user_params)
+  #end
 
-  private
-  def user_params
-    params.require(:user).permit(:nickname, :email, :password, :sei, :mei, :kanasei, :kananei, :birthday)
-  end
+  #private
+  #def user_params
+  #  params.require(:user).permit(:nickname, :email, :password, :sei, :mei, :kanasei, :kananei, :birthday)
+  #end
 end
