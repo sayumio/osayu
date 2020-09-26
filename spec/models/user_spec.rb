afbd61b38aa3aa6283e74ee71387dabd4c8cd212
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-26 18:38:24 +0900

    記述の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,103 +20,88 @@ RSpec.describe User, type: :model do
 
     context '新規登録がうまくいかないとき' do
       it "nicknameが空だと登録できない" do
-        user = FactoryBot.build(:user)
-        user.nickname = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("ニックネームを入力してください")
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
 
       it "emailが空では登録できない" do
-        user = FactoryBot.build(:user)
-        user.email = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("Eメールを入力してください")
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
 
       it "パスワードが空では登録できない" do
-        user = FactoryBot.build(:user)
-        user.password = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワードを入力してください")
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
 
       it "パスワードの確認が空では登録できない" do
-        user = FactoryBot.build(:user)
-        user.password_confirmation = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
 
       it "パスワードの確認がパスワードと違うと登録できない" do
-        user = FactoryBot.build(:user)
-        user.password_confirmation = "abcd0000"
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+        @user.password_confirmation = "abcd0000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
 
       it "seiが空では登録できない"do
-        user = FactoryBot.build(:user)
-        user.sei = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("姓を入力してください")
+        @user.sei = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("姓を入力してください")
       end
 
       it "meiが空では登録できない"do
-        user = FactoryBot.build(:user)
-        user.mei = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("名を入力してください")
+        @user.mei = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名を入力してください")
       end
 
       it "kanaseiが空では登録できない"do
-        user = FactoryBot.build(:user)
-        user.kanasei = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("姓（カナ）を入力してください")
+        @user.kanasei = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("姓（カナ）を入力してください")
       end
 
       it "kanameiが空では登録できない"do
-        user = FactoryBot.build(:user)
-        user.kanamei = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("名（カナ）を入力してください")
+        @user.kanamei = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名（カナ）を入力してください")
       end
 
       it "bitrhdayが空では登録できない"do
-        user = FactoryBot.build(:user)
-        user.birthday = ""
-        user.valid?
-        expect(user.errors.full_messages).to include("生年月日を入力してください")
+        @user.birthday = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("生年月日を入力してください")
       end
 
       it "メールアドレスに＠がないと登録できない"do
-        user = FactoryBot.build(:user)
-        user.email = "kkkgmail.com"
-        user.valid?
+        @user.email = "kkkgmail.com"
+        @user.valid?
       end
 
       it "パスワードが6文字以下では登録できない"do
-        user = FactoryBot.build(:user)
-        user.password = "abc00"
-        user.valid?
+        @user.password = "abc00"
+        @user.valid?
       end
 
       it "パスワードが半角英数字混合でないと登録できない"do
-        user = FactoryBot.build(:user)
-        user.password = "abcdefg"
-        user.valid?
+        @user.password = "abcdefg"
+        @user.valid?
       end
 
       it "ユーザー本名は全角で入力でないと登録できない"do
-        user = FactoryBot.build(:user)
-        user.sei = "ﾔﾏﾀﾞ"
-        user.valid?
+        @user.sei = "ﾔﾏﾀﾞ"
+        @user.valid?
       end
 
       it "ユーザー本名のフリガナはカタカナでないと登録できない"do
-        user = FactoryBot.build(:user)
-        user.kanasei = "やまだ"
-        user.valid?
+        @user.kanasei = "やまだ"
+        @user.valid?
       end
     end
   end
