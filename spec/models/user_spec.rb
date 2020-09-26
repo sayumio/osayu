28a7aa720417503dc03632e694b89cad8dfaa400
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-26 16:02:38 +0900

    factorybotの記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
-describe User do
-  before do
-    @user = FactoryBot.build(:user)
-  end
+RSpec.describe User, type: :model do
+  describe User do
+    before do
+      @user = FactoryBot.build(:user)
+    end
 
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
