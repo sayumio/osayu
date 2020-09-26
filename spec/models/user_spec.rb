168be8b51ffbf2a376c3a6322fcd9cdc3b03a52c
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-26 17:13:25 +0900

    テストコード訂正とrequireインストール

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,79 +5,119 @@ RSpec.describe User, type: :model do
       @user = FactoryBot.build(:user)
     end
 
-  describe 'ユーザー新規登録' do
-    context '新規登録がうまくいくとき' do
-      it "nickname,email、passwordとpassword_confirmation、sei,mei,kanasei,kanamei,birthdayが存在すれば登録できる" do
-      end
+    describe 'ユーザー新規登録' do
+      context '新規登録がうまくいくとき' do
+        it "nickname,email、passwordとpassword_confirmation、sei,mei,kanasei,kanamei,birthdayが存在すれば登録できる" do
+          expect(@user).to be_valid
+        end
 
-      it "passwordが6文字以上で登録できる" do
+        it "passwordが6文字以上で登録できる" do
+          @user.password = "abc0000"
+          expect(@user).to be_valid
+        end
       end
     end
 
     context '新規登録がうまくいかないとき' do
       it "nicknameが空だと登録できない" do
-        user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
+        user = FactoryBot.build(:user)
+        user.nickname = ""
         user.valid?
+        expect(user.errors.full_messages).to include("Nickname can't be blank")
       end
 
       it "emailが空では登録できない" do
-        user = User.new(nickname: "タロウ", email: "", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
+        user = FactoryBot.build(:user)
+        user.email = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("email can't be blank")
+      end
+
+      it "パスワードが空では登録できない" do
+        user = FactoryBot.build(:user)
+        user.password = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("password can't be blank")
+      end
+
+      it "パスワードの確認が空では登録できない" do
+        user = FactoryBot.build(:user)
+        user.password_confirmation = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("password_confirmation can't be blank")
+      end
+
+      it "パスワードの確認がパスワードと違うと登録できない" do
+        user = FactoryBot.build(:user)
+        user.password_confirmation = "abcdoooo"
+        user.valid?
+        expect(user.errors.full_messages).to include("password_confirmation can't be blank")
+      end
+
+      it "seiが空では登録できない"do
+        user = FactoryBot.build(:user)
+        user.sei = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("sei can't be blank")
+      end
+
+      it "meiが空では登録できない"do
+        user = FactoryBot.build(:user)
+        user.mei = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("mei can't be blank")
+      end
+
+      it "kanaseiが空では登録できない"do
+        user = FactoryBot.build(:user)
+        user.kansei = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("kanasei can't be blank")
+      end
+
+      it "kanameiが空では登録できない"do
+        user = FactoryBot.build(:user)
+        user.kanamei = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("kanamei can't be blank")
+      end
+
+      it "bitrhdayが空では登録できない"do
+        user = FactoryBot.build(:user)
+        user.birthday = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("birthday can't be blank")
+      end
+
+      it "メールアドレスに＠がないと登録できない"do
+        user = FactoryBot.build(:user)
+        user.email = "kkkgmail.com"
         user.valid?
       end
 
-    it "パスワードが空では登録できない" do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "", password_confirmation: "", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
+      it "パスワードが6文字以下では登録できない"do
+        user = FactoryBot.build(:user)
+        user.password = "abc00"
+        user.valid?
+      end
 
-    it "seiが空では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
+      it "パスワードが半角英数字混合でないと登録できない"do
+        user = FactoryBot.build(:user)
+        user.password = "abcdefg"
+        user.valid?
+      end
 
-    it "meiが空では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
+      it "ユーザー本名は全角で入力でないと登録できない"do
+        user = FactoryBot.build(:user)
+        user.sei = "ﾔﾏﾀﾞ"
+        user.valid?
+      end
 
-    it "kanaseiが空では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "kanameiが空では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "bitrhdayが空では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "")
-      user.valid?
-    end
-
-    it "メールアドレスに＠がないと登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkkgmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "パスワードが6文字以下では登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "0000", password_confirmation: "0000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "パスワードが半角英数字混合でないと登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "ユーザー本名は全角で入力でないと登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "yamada", mei: "太郎", kanasei: "ヤマダ", kanamei: "タロウ", birthday: "1990/01/01")
-      user.valid?
-    end
-
-    it "ユーザー本名のフリガナはカタカナでないと登録できない"do
-      user = User.new(nickname: "タロウ", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", sei: "山田", mei: "太郎", kanasei: "やまだ", kanamei: "たろう", birthday: "1990/01/01")
-      user.valid?
+      it "ユーザー本名のフリガナはカタカナでないと登録できない"do
+        user = FactoryBot.build(:user)
+        user.kanasei = "やまだ"
+        user.valid?
+      end
     end
   end
 end
