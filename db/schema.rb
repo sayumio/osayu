42fa44e3d5c9a9c34c0939f408b5eaac64c03757
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-26 21:59:02 +0900

    itemマイグレーション記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_122939) do
+ActiveRecord::Schema.define(version: 2020_09_26_123016) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "charges_id", null: false
+    t.integer "area_id", null: false
+    t.integer "days_id", null: false
+    t.integer "price", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
