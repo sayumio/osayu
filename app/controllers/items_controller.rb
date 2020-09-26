a0e8c30c630e041cd6767b2f5ed34a995dc1b235
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-26 22:02:34 +0900

    インスタンス変数にItemテーブルを代入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,11 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
