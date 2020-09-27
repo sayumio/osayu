bd8f27267453683dbfe500a1fbc06f6fb45015dd
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-27 20:08:21 +0900

    商品情報をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order( "created_at DESC" )
   end
 
   def new
@@ -8,11 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :index
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit( :name, :image, :comment, :category_id, :status_id, :charges_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit( :name, :comment, :price, :category_id, :status_id, :charges_id, :area_id, :days_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category_id, :status_id, :charges_id, :area_id, :days_id
-
-  validates :name, :comment, :category_id, :status_id, :charges_id, :area_id, :days_id, :price, :user_id, presence: true
-
-  validates :category_id, :status_id, :charges_id, :area_id, :days_id, numericality: { other_than: 1 }
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
 
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  validates :name, :comment, :price, :user_id, presence: true
+
+  validates :category_id, :status_id, :charges_id, :area_id, :days_id, numericality: { other_than: 1 }
 
 end
