76515e8d878acee6456f394b0a030b8cb2ddb30a
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-27 23:34:10 +0900

    エラーハンドリングとJavaScriptの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order( "created_at DESC" )
+    @items = Item.includes(:order).order( "created_at DESC" )
   end
 
   def new
@@ -10,14 +10,14 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to items_path
+      redirect_to root_path
     else
-      render :index
+      render action: :new
     end
   end
 
   private
   def item_params
-    params.require(:item).permit( :name, :comment, :price, :category_id, :status_id, :charges_id, :area_id, :days_id).merge(user_id: current_user.id)
+    params.require(:item).permit( :image, :name, :comment, :price, :category_id, :status_id, :charges_id, :area_id, :days_id).merge(user_id: current_user.id)
   end
 end
