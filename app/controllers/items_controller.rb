de07b5129ffc3b65915375aa28b0c661d738927f
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-28 17:27:41 +0900

    一覧ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,22 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+<<<<<<< Updated upstream
   def index
     @items = Item.all#.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+=======
+  before_action :set_item, only: [:show ]
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new(item_params)
+>>>>>>> Stashed changes
   end
 
   def create
@@ -18,9 +29,23 @@ class ItemsController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :comment, :price, :category_id, :status_id, :charges_id, :area_id, :days_id).merge(user_id: current_user.id)
+=======
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :comment, :price, :category_id, :status_id, :charges_id, :area_id, :days_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+>>>>>>> Stashed changes
   end
 end
