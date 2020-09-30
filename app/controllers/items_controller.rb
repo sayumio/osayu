6ff845dfc0e33b8ed50e6253aaa154ba32daa065
sayumio <s83.oshima.sayumi@gmail.com>
2020-09-30 12:09:24 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -30,6 +30,15 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if  @item.destroy
+      redirect_to root_path
+   else
+     render :show
+    end
+  end
+
 
   private
 
